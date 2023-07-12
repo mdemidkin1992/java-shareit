a6d1bee7e5d63748d9b21be9a947c903c6bef592
mdemidkin <mdemidkin@yandex.ru>
2023-07-12 16:07:10 +0200

    feature: microservice setup server/gateway 1

---
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookItemRequestDto {
+	private long itemId;
+	@FutureOrPresent
+	private LocalDateTime start;
+	@Future
+	private LocalDateTime end;
+}
