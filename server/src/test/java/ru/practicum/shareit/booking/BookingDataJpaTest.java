8994aaa5d5bc192beda2ad686cc6effb2aae9d54
mdemidkin <mdemidkin@yandex.ru>
2023-07-17 18:34:17 +0200

    cleanup: removed unused imports

--- server/src/test/java/ru/practicum/shareit/booking/BookingDataJpaTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingDataJpaTest.java
@@ -18,7 +18,6 @@ import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
